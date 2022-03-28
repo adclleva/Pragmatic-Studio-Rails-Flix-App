02d7589cddbba88dc48060581d6b032e97aaa879
adclleva <arvin.lleva@yahoo.com>
2022-03-27 23:13:54 -0700

    generatated a movie model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_28_061256) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.decimal "total_gross"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
